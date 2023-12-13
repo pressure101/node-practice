af0e55aa71de8c5527b99478fe913901bb7ce33e
Cavasier Morris <cavamorr@gmail.com>
2023-12-12 23:57:50 -0600

    adding server

--- twitter-bot/src/index.js
+++ twitter-bot/src/index.js
@@ -1,1 +1,8 @@
-// TODO: determine the necessity of index file
+var http = require('http')
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type':'text/plain'})
+    response.end('Hello you beautiful soul.\n')
+}).listen(8080)
+
+console.log('Server running on port 8080')
